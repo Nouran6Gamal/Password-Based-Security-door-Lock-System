14614bf5ae50bb9e6669b62e80f2ddce1adc503c
Nouran Gamal <g_nouran@yahoo.com>
2022-05-20 20:19:36 +0200

    Add files via upload

---
+++ Control/Debug/Control.d
@@ -0,0 +1,17 @@
+Control.o Control.o: ../Control.c ../uart.h ../micro_config.h \
+  ../std_types.h ../common_macros.h ../i2c.h ../external_eeprom.h \
+  ../timer0.h
+
+../uart.h:
+
+../micro_config.h:
+
+../std_types.h:
+
+../common_macros.h:
+
+../i2c.h:
+
+../external_eeprom.h:
+
+../timer0.h:
